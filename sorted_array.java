92a1ee96201dfeba0a84631712b37df96dfbbc16
surbhisaini123 <shurbhisaini206@gmail.com>
2025-06-03 23:47:30 +0530

    DSA

--- sorted_array.java
+++ sorted_array.java
@@ -1,23 +1,48 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+
 public class sorted_array { 
+    static void MergeTwoArray(){
+    
+    ArrayList <Integer>arr1=new ArrayList<>();
+       arr1.add(5);
+       arr1.add(9);
+       arr1.add(3);
+       arr1.add(1);
+       arr1.add(2);
+       Collections.sort(arr1);
+       System.out.println("Element of array 1----->");
+       System.out.println(arr1); 
+       System.out.println(" ");
+
+    ArrayList <Integer>arr2=new ArrayList<>();
+       arr2.add(8);
+       arr2.add(11);
+       arr2.add(55);
+       arr2.add(87);
+       arr2.add(9);
+       Collections.sort(arr2);
+       System.out.println("Element of array 2----->");
+       System.out.println(arr2);
+       System.out.println(" ");
+
+      // To Print Merge Two Array======>
+       arr1.addAll(arr2);
+       System.out.println("MERGE TWO SORTED ARRAY===: "+arr1);
+        System.out.println(" ");
+
+    
+      //To Print The Sorted Form Of Merge Array=======>
+       Collections.sort(arr1);
+       System.out.println("SORTED FORM OF MERGE ARRAY===: " +arr1);
+
+
+    }
     public static void main(String[] args) {
-       // int arr[]={25,1,9,4,6,8};
-        int arr[]={1,2,3,4,5};
-        int i=0;
-        int j=i+1;
-        int check=0;
-        while(i<=j){
-            if(arr[i]<arr[j]){
-                System.out.println("sorted");
-            }
-            else{
-                System.out.println("no");
-            }
-            i++;
-            j--;
-            check++;
-        }
-       
-    
-}
-    
+        MergeTwoArray();
+
+
+    }
 }
