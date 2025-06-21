91b6782b74d9e3277476ac3666cd77e1734f8bc2
surbhisaini123 <shurbhisaini206@gmail.com>
2025-06-21 23:46:57 +0530

    DSA

---
+++ reverse_str.java
@@ -0,0 +1,12 @@
+public class reverse_str {
+    public static void main(String[] args) {
+        String S="hello";
+
+       char[] ch=S.toCharArray();
+        for(int i=0;i<=S.length();i++){
+            System.out.println(ch);
+        }
+            
+    }
+    
+}
