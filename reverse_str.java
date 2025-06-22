163276e1e4e2d42be403860e190fe2405119f324
Surbhi Saini <shurbhisaini2006@gmail.com>
2025-06-22 23:09:01 +0530

    Add files via upload

--- reverse_str.java
+++ reverse_str.java
@@ -1,11 +1,19 @@
+
 public class reverse_str {
     public static void main(String[] args) {
         String S="hello";
 
-       char[] ch=S.toCharArray();
-        for(int i=0;i<=S.length();i++){
-            System.out.println(ch);
+       char[] ch=S.toCharArray();//System.out.println(ch);
+       System.out.println("ORIGINAL STRING:");
+        for(int i=0;i<ch.length;i++){
+            System.out.println(ch[i]);
         }
+        System.out.println("---//---//---//---//");
+        System.out.println("REVERSE STRING:");
+        for(int i=ch.length-1;i>=0;i--){
+            System.out.println(ch[i]);
+        }
+      
             
     }
     
